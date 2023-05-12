0b5baec842d807e5c438c8f83cb2b675c9a95fd3
JeroBl <jeroblanco44@gmail.com>
2023-05-11 21:31:07 -0300

    11.6

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Home,Landing,Detail,Form} from "./views";
+import { Route } from 'react-router-dom';
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <NavBar/>
+      <Route exact path="/" render={() => <Landing/>} />
+      <Route path="/home" render={() => <Home/>} />
+      <Route exact path="/detail" render={() => <Detail/>} />
+      <Route exact path="/create" render={() => <Form/>} />
+      
+        
+
+      
+      
     </div>
   );
 }
