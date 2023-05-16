c34747a8513b280ce3c4d6cee6e508be722e8537
JeroBl <jeroblanco44@gmail.com>
2023-05-16 10:35:15 -0300

    16.0

---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import style from "./SearchBar.module.css"; 
+
+const SearchBar = () => {
+  const [inputValue, setInputValue] = useState("");
+  const history = useHistory();
+
+  const enterSearch = (event) => {
+    if (event.key === "Enter") {
+      navigateToSearch();
+    }
+  };
+
+  const clickSearch = () => {
+    navigateToSearch();
+  };
+
+  const navigateToSearch = () => {
+    const queryParams = { name: inputValue };
+    const search = new URLSearchParams(queryParams).toString();
+    history.push(`/name?${search}`);
+  };
+
+  const inputHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <section className={style.mainContainer}>
+      <input
+        className={style.searchInput}
+        onChange={inputHandler}
+        onKeyDown={enterSearch}
+        type="text"
+        placeholder="Inserte el nombre"
+      />
+      <button className={style.searchButton} onClick={clickSearch}>
+        Buscar
+      </button>
+    </section>
+  );
+};
+
+export default SearchBar;
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -2,6 +2,7 @@ import CardsContainer from "../../components/CardsContainer/CardsContainer";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getDogs} from "../../redux/actions";
+import SearchBar from "../../components/SearchBar/SearchBar";
 
 
 const Home = () => {
@@ -15,6 +16,7 @@ const Home = () => {
     return(
         <>
             <h1>Esta es la Home</h1>
+            <SearchBar/>
             <CardsContainer/>
         </>
     )
